f7bc3e50f2205bbf186132bd052e5df77586e547
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-11-21 13:59:00 -0800

    Finish Sign up page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: calc(100vh - 56px);
@@ -20,17 +21,47 @@ const Wrapper = styled.div`
   gap: 10px;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+`;
 
-const SubTitle = styled.h2``;
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-const More = styled.div``;
+const More = styled.div`
+  display: flex;
+  margin-top: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-const Links = styled.span``;
+const Links = styled.div`
+  margin-left: 50px;
+`;
+
+const Link = styled.span`
+  margin-left: 30px;
+`;
 
 const SignIn = () => {
   return (
