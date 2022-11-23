345793a23940747d50dc0202f25bc90a512f7cfb
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-11-23 00:05:01 -0800

    Add auth files

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,3 +14,16 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send('User has been created!');
+  } catch (error) {
+    next(error);
+  }
+};
